06d481aeca1b4e9b53b6b7c1a3c84e368cccd6ba
Steve Kisinga <stevekisinga@gmail.com>
2022-09-21 17:04:50 +0300

    kinda waorking

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
 
-var ina219 = require('easybotics-ina219');
-
-ina219.init();
-ina219.enableLogging(true);
+var solar = require('easybotics-ina219');
+var mains = require('easybotics-ina219');
 
 
-setInterval(()=>{
-  ina219.calibrate32V1A(function () {
-  
-    ina219.getBusVoltage_V(function (volts) {
+mains.init(undefined, 3);
+solar.init();
+
+solar.calibrate32V1A(function () {
+
+})
+
+mains.calibrate32V1A(function () {
+  mains.getBusVoltage_V(function (volts) {
     
-      console.log("Voltage: " + volts);
-      ina219.getCurrent_mA(function (current){
-        
-        console.log("Current (mA): " + current + "\n\n");
-      });	
-    });
+    console.log("Voltage: " + volts);
+    mains.getCurrent_mA(function (current){
+      
+      console.log("Current (mA): " + current + "\n\n");
+    });	
   });
-}, 2000) 
+})
+
+solar.calibrate32V1A(function () {
+  solar.getBusVoltage_V(function (volts) {
+    
+    console.log("Voltage: " + volts);
+    solar.getCurrent_mA(function (current){
+      
+      console.log("Current (mA): " + current + "\n\n");
+    });	
+  });
+})
+
+function readFromSolar(){
+  
+}
+
+function readFromMains(){
+
+}
+
+function read(){
+
+}
+// setInterval(()=>{
+//   ina219.calibrate32V1A(function () {
+  
+//     ina219.getBusVoltage_V(function (volts) {
+    
+//       console.log("Voltage: " + volts);
+//       ina219.getCurrent_mA(function (current){
+        
+//         console.log("Current (mA): " + current + "\n\n");
+//       });	
+//     });
+//   });
+// }, 2000) 
 
